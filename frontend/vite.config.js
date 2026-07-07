6939ff567866bbed37065cd6cf711da41ab22b64
demianashawky11-tech <demiana.shawky11@gmail.com>
2026-07-07 09:08:32 +0300

    Add base path to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // إضافة هذا السطر يضمن أن الموقع يبحث عن ملفاته في المسارات الصحيحة
+  base: './', 
 })
